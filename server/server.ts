00344176e9d2076b926ee9cb8b8db539343984c3
conmeohudon <nguyenthekien8204@gmailcom>
2026-02-11 12:59:30 +0700

    session and cookies properties update

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ const app = express();
 app.disable('etag');
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblifyy.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblifyy.vercel.app'],
     credentials: true
 }))
 
