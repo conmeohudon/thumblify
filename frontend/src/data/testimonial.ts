a0efe5b70001f7660e7ea49e26fa880f548604b5
conmeohudon <nguyenthekien8204@gmailcom>
2026-02-11 12:21:25 +0700

    oki

---
+++ frontend/src/data/testimonial.ts
@@ -0,0 +1,32 @@
+import type { ITestimonial } from "../types";
+
+export const testimonialsData: ITestimonial[] = [
+    {
+        image: 'https://i.ibb.co/Y7SYkPXV/photo-21-2024-12-25-16-57-52.jpg',
+        name: 'Diem De',
+        handle: '@diemxinhnhunghoide',
+        date: 'February 14, 2025',
+        quote: 'wowowow vippro qua de',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200',
+        name: 'Ethan Walker',
+        handle: '@ethanwrites',
+        date: 'March 3, 2025',
+        quote: 'We’ve tried several tools, but nothing comes close in terms of speed and simplicity. Absolute game-changer.',
+    },
+    {
+        image: 'https://i.ibb.co/Jw7F9sjK/photo-1-2024-12-30-20-25-32.jpg',
+        name: 'Dimz',
+        handle: '@dimz',
+        date: 'April 22, 2025',
+        quote: 'The automation features alone have saved our team countless hours every week. Worth every penny.',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60',
+        name: 'Liam Brooks',
+        handle: '@liambrooks',
+        date: 'May 18, 2025',
+        quote: 'Setup was ridiculously easy. Within 10 minutes, we were running live and onboarding our first customers.',
+    },
+];
---
+++ server/server.ts
@@ -0,0 +1,65 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import AuthRouter from './routes/AuthRoutes';
+import ThumbnailRouter from './routes/ThumbnailRoutes';
+import UserRouter from './routes/UserRoutes';
+
+declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string
+    }
+}
+
+const app = express();
+
+app.disable('etag');
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true
+}))
+
+app.use(express.json())
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}, // 7 days
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL as string,
+        collectionName: 'sessions'
+    })
+}))
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.use('/api/auth', AuthRouter)
+
+app.use('/api/thumbnail', ThumbnailRouter)
+
+app.use('/api/user', UserRouter)
+
+const port = process.env.PORT || 3000;
+
+// Gọi connectDB và listen server trong async function
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to database:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
